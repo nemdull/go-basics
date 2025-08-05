68ea60a9e00f072aaf7b729e4e708b6e6994417b
高橋大典 <takahashidaisuke@makku.local>
2025-08-05 23:31:35 +0900

    Add: slice

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
+import "fmt"
 
 func main() {
-	var ui1 uint16
-	fmt.Printf("memory address of ui1: %p\n", &ui1)
-	var ui2 uint16
-	fmt.Printf("memory address of ui2: %p\n", &ui2)
-	var p1 *uint16
-	fmt.Printf("value of p1: %v\n", p1)
-	p1 = &ui1
-	fmt.Printf("value of p1: %v\n", p1)
-	fmt.Printf("size of p1: %d[bytes]\n", unsafe.Sizeof(p1))
-	fmt.Printf("memory address of p1: %p\n", &p1)
-	fmt.Printf("value of ui1(dereference): %v\n", *p1)
-	*p1 = 1
-	fmt.Printf("value of ui1(dereference): %v\n", *p1)
+	var a1 [3]int
+	var a2 = [3]int{10, 20, 30}
+	a3 := [...]int{10, 20}
+	fmt.Printf("%v %v %v\n", a1, a2, a3)
+	fmt.Printf("%v %v\n", len(a3), cap(a3))
 
-	var pp1 **uint16 = &p1
-	fmt.Printf("value of pp1: %v\n", pp1)
-
-	ok, result := true, "A"
-	fmt.Printf("memory address of p1: %p\n", &result)
-	if ok {
-		result = "B"
-		fmt.Printf("memory address of p1: %p\n", &result)
-		println(result)
-
-	} else {
-		result = "C"
-		println(result)
-	}
-	println(result)
 }
