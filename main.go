54022eee64f9b0bde0a77e2f8145b74b7ca88b49
nemdull <dlanx@outlook.jp>
2025-08-06 23:47:38 +0900

    Add: nil slice

--- main.go
+++ main.go
@@ -8,5 +8,12 @@ func main() {
 	a3 := [...]int{10, 20}
 	fmt.Printf("%v %v %v\n", a1, a2, a3)
 	fmt.Printf("%v %v\n", len(a3), cap(a3))
+	fmt.Printf("%T %T\n", a2, a3)
 
+	var s1 []int
+	s2 := []int{}
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	fmt.Printf("s2: %[1]T %[1]v %v %v\n", s2, len(s2), cap(s2))
+	fmt.Println(s1 == nil)
+	fmt.Println(s2 == nil)
 }
