cb0bafbd61dbe19ec5f1b8162347728212bddec7
nemdull <dlanx@outlook.jp>
2025-08-16 23:46:57 +0900

    docs: Add detailed comments to all Go files

--- calculator/multiply.go
+++ calculator/multiply.go
@@ -1,5 +1,6 @@
 package calculator
 
+// Multiply は2つの float64 型の数値を引数として受け取り、その積を返します。
 func Multiply(a float64, b float64) float64 {
 	return a * b
 }
--- calculator/sum.go
+++ calculator/sum.go
@@ -1,5 +1,6 @@
 package calculator
 
+// Sum は2つの float64 型の数値を引数として受け取り、その合計を返します。
 func Sum(a float64, b float64) float64 {
 	return a + b
 }
